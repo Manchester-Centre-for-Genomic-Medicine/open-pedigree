44424e1ac9ea8e14ab217775217445fd48bac10c
Algy Taylor <algy.taylor@mft.nhs.uk>
2021-11-11 15:39:46 +0000

    set live environment details

--- src/app.js
+++ src/app.js
@@ -18,9 +18,9 @@ document.observe('dom:loaded', async function () {
   console.log(Object.keys(process.env));
   const configureAuth0 = async () => {
     auth0 = await new Auth0Client({
-      domain: "gen-o-dev.eu.auth0.com",
-      client_id: "d3YJUQgU53bhu4O7nhPtFnXM4LjNUb6U",
-      audience: "https://gen-o-dev.eu.auth0.com/api/v2/",
+      domain: "gen-o.eu.auth0.com",
+      client_id: "cMDwFfxF4hC1GOs6W35HdDSPmregh6A7",
+      audience: "https://gen-o.eu.auth0.com/api/v2/",
     });
   };
 
@@ -45,7 +45,7 @@ document.observe('dom:loaded', async function () {
   const graphql = async (body) => {
     const token = await auth0.getTokenSilently();
 
-    const result = await fetch("https://develop-graphql.northwestglh.com/v1/graphql", {
+    const result = await fetch("https://graphql.northwestglh.com/v1/graphql", {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`,
