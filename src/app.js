9102156eae02302b8f0c42ae58098e790a7c7067
Algy Taylor <algy.taylor@mft.nhs.uk>
2022-06-16 09:47:11 +0100

    init: link to dev version of app

--- src/app.js
+++ src/app.js
@@ -15,12 +15,11 @@ import '../public/vendor/phenotips/Skin.css';
 
 document.observe('dom:loaded', async function () {
   let auth0 = null;
-  console.log(Object.keys(process.env));
   const configureAuth0 = async () => {
     auth0 = await new Auth0Client({
-      domain: "gen-o.eu.auth0.com",
-      client_id: "cMDwFfxF4hC1GOs6W35HdDSPmregh6A7",
-      audience: "https://gen-o.eu.auth0.com/api/v2/",
+      domain: "gen-o-dev.eu.auth0.com",
+      client_id: "d3YJUQgU53bhu4O7nhPtFnXM4LjNUb6U",
+      audience: "https://gen-o-dev.eu.auth0.com/api/v2/",
     });
   };
 
@@ -45,7 +44,7 @@ document.observe('dom:loaded', async function () {
   const graphql = async (body) => {
     const token = await auth0.getTokenSilently();
 
-    const result = await fetch("https://graphql.northwestglh.com/v1/graphql", {
+    const result = await fetch("http://localhost:4100/v1/graphql", {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`,
