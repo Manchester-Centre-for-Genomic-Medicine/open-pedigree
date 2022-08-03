2627937bde805246a05edf688369903e47181649
niab <nikita.abramovs@gmail.com>
2022-08-03 16:15:00 +0100

    go-1385: linked NHS PDS API.
    On external identifier (NHS number) update in the pedigree node menu a request is first sent to Gen-O in attempt to get an individual demographics data. If the individual is not found in Gen-O, then open pedigree tries to obtain this data from NHS Digital PDS FHIR.

--- src/app.js
+++ src/app.js
@@ -25,14 +25,14 @@ document.observe('dom:loaded', async function () {
       //audience: "https://gen-o.eu.auth0.com/api/v2/",
 
       // TEST
-      //domain: "gen-o-test.eu.auth0.com",
-      //client_id: "Kx350GeJFnWb1mYc5H3GjMvG8hrc2OYR",
-      //audience: "https://gen-o-test.eu.auth0.com/api/v2/",
+      domain: "gen-o-test.eu.auth0.com",
+      client_id: "Kx350GeJFnWb1mYc5H3GjMvG8hrc2OYR",
+      audience: "https://gen-o-test.eu.auth0.com/api/v2/",
 
       // DEVELOP
-      domain: "gen-o-dev.eu.auth0.com",
-      client_id: "d3YJUQgU53bhu4O7nhPtFnXM4LjNUb6U",
-      audience: "https://gen-o-dev.eu.auth0.com/api/v2/",
+      //domain: "gen-o-dev.eu.auth0.com",
+      //client_id: "d3YJUQgU53bhu4O7nhPtFnXM4LjNUb6U",
+      //audience: "https://gen-o-dev.eu.auth0.com/api/v2/",
     });
   };
 
@@ -137,8 +137,7 @@ document.observe('dom:loaded', async function () {
     } 
   });
 
-  // hook externalid up to the gen-o database
-  document.observe('pedigree:person:set:externalid', async (event) => {
+  const getDemographicsGenO = async function (nhsID) {
     const query = `
       query GetDemographics(
         $primaryIdentifier: String!
@@ -166,33 +165,103 @@ document.observe('dom:loaded', async function () {
         }
       }
     `;
-
     const variables = {
-      primaryIdentifier: event.memo.value,
+      primaryIdentifier: nhsID,
     };
     const result = await graphql({ query, variables });
-    event.memo.node.setFirstName(
-      result.data?.individual[0]?.first_name
-    );
-    event.memo.node.setLastName(
-      result.data?.individual[0]?.last_name
-    );
-    event.memo.node.setLifeStatus(
-      result.data?.individual[0]?.deceased
-        ? 'deceased'
-        : 'alive'
-    );
-    event.memo.node.setBirthDate(
-      new Date(result.data?.individual[0]?.date_of_birth)
-    );
-    event.memo.node.setDeathDate(
-      new Date(result.data?.individual[0]?.date_of_death)
-    );
-    var hpos = [];
-    result.data?.individual[0]?.phenopacket?.phenotypic_features?.each(function(v) {
-      hpos.push(new HPOTerm(v.hpo.id, v.hpo.name));
-    });
-    event.memo.node.setHPO(hpos);
+    return result;
+  }
+
+  const getDemographicsPDS = async function (nhsID) {
+    const query = `
+      query GetPatientDemographicsFromSpine($nhsNumber:String!) {
+        individual:getPatientFromFHIR(id:$nhsNumber) {
+          birthDate
+          deceased
+          deceasedDateTime
+          name {
+            given
+            family
+            period {
+              start
+            }
+          }
+          gender
+        }
+      }
+    `;
+    const variables = {
+      nhsNumber: nhsID,
+    };
+    const result = await graphql({ query, variables });
+    return result;
+  }
+
+  function clearNodeDemographics(node) {
+    node.setFirstName('');
+    node.setLastName('');
+    node.setLifeStatus('alive');
+    node.setBirthDate('');
+    node.setDeathDate('');
+    node.setGender('U');
+    node.setHPO([]);
+    node.setGenes([]);
+    node.setDisorders([]);    
+  }
+
+  // hook externalid up to the gen-o database
+  document.observe('pedigree:person:set:externalid', async (event) => {
+    var result = await getDemographicsGenO(event.memo.value);  
+    if (result.data?.individual[0]) {
+      clearNodeDemographics(event.memo.node);
+      event.memo.node.setFirstName(
+        result.data?.individual[0]?.first_name
+      );
+      event.memo.node.setLastName(
+        result.data?.individual[0]?.last_name
+      );
+      event.memo.node.setLifeStatus(
+        result.data?.individual[0]?.deceased
+          ? 'deceased'
+          : 'alive'
+      );
+      event.memo.node.setBirthDate(
+        new Date(result.data?.individual[0]?.date_of_birth)
+      );
+      event.memo.node.setDeathDate(
+        new Date(result.data?.individual[0]?.date_of_death)
+      );
+      var hpos = [];
+      result.data?.individual[0]?.phenopacket?.phenotypic_features?.each(function(v) {
+        hpos.push(new HPOTerm(v.hpo.id, v.hpo.name));
+      });
+      event.memo.node.setGender(
+        result.data?.individual[0]?.sex
+      );
+      event.memo.node.setHPO(hpos);
+    } else {
+      var result = await getDemographicsPDS(event.memo.value);
+      if (result.data?.individual) {
+        clearNodeDemographics(event.memo.node);
+        var names = result.data.individual.name.sort(function(a, b) {
+          return new Date(b.period.start) - new Date(a.period.start);
+        });
+        event.memo.node.setFirstName(names[0]?.given[0]);
+        event.memo.node.setLastName(names[0]?.family);
+        event.memo.node.setLifeStatus(
+          result.data.individual?.deceased
+            ? 'deceased'
+            : 'alive'
+        );
+        event.memo.node.setBirthDate(
+          new Date(result.data.individual?.birthDate)
+        );
+        event.memo.node.setDeathDate(
+          new Date(result.data.individual?.deceasedDateTime)
+        );
+        event.memo.node.setGender(result.data.individual.gender);
+      }
+    }
     editor.getNodeMenu().update();
   });
 });
