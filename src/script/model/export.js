8a9d74a97652372e8ff4399a659526fe16b2644e
Algy Taylor <algy.taylor@mft.nhs.uk>
2025-05-14 11:21:55 +0100

    gh-1020: add canrisk 3 exporter
    Update the `PedigreeExport` class to add an `exportAsCanrisk` function
    that allows a pedigree to be exported in CanRisk 3.0 format.

--- src/script/model/export.js
+++ src/script/model/export.js
@@ -82,6 +82,187 @@ PedigreeExport.exportAsPED = function(pedigree, idGenerationPreference) {
   return output;
 };
 
+function getAge(dateOfBirth) {
+  const today = new Date(); // Get the current date
+  let age = today.getFullYear() - dateOfBirth.getFullYear(); // Calculate the year difference
+
+  // Adjust the age if the birthday hasn't occurred yet this year
+  const hasHadBirthdayThisYear =
+    today.getMonth() > dateOfBirth.getMonth() ||
+    (today.getMonth() === dateOfBirth.getMonth() && today.getDate() >= dateOfBirth.getDate());
+
+  if (!hasHadBirthdayThisYear) {
+    age--;
+  }
+
+  return age;
+}
+
+/**
+ * Get the twin group number for an individual.
+ * 
+ * @param {Object} individual - The individual object containing twin group information.
+ * @returns {number|string} - The twin group number (0-9 or 'A') or 0 if not a twin.
+ * @throws {Error} - Throws an error if the twin group number is invalid.
+ */
+function getTwinGroup(individual) {
+  if (individual.hasOwnProperty('monozygotic') && individual['monozygotic'] === true) {
+    if (!individual.hasOwnProperty('twinGroup')) {
+      console.warn('Twin group number is missing, setting to 0');
+      return 0;
+    }
+
+    if (individual['twinGroup'] <= 8) {
+      return individual['twinGroup'] + 1;
+    } else if (individual['twinGroup'] === 9) {
+      return 'A';
+    } else {
+      throw new Error('Too many twin groups for CanRisk support');
+    }
+  }
+
+  return 0;
+}
+
+/**
+ * Canrisk format
+ * Details: https://canrisk.atlassian.net/wiki/spaces/FAQS/pages/2456616961/CanRisk+Pedigree+Data+v3+File+Format+Specification
+ * 
+ * CanRisk format is similar to PED format, but with the following differences:
+ * 
+ */
+
+PedigreeExport.exportAsCanrisk = function(pedigree, idGenerationPreference) {
+  const generateDataLine = (values) => values.join('\t');
+  const generateHeader = (values) => values
+      .map(val => `##${val}`)
+      .join('\n') + '\n';
+  const HEADER = generateHeader([
+    'CanRisk 3.0',
+    generateDataLine([
+      'FamID',
+      'Name',
+      'Target',
+      'IndivID',
+      'FathID',
+      'MothID',
+      'Sex',
+      'MZtwin',
+      'Dead',
+      'Age',
+      'Yob',
+      'BC1',
+      'BC2',
+      'OC',
+      'PRO',
+      'PAN',
+      'Ashkn',
+      'BRCA1',
+      'BRCA2',
+      'PALB2',
+      'ATM',
+      'CHEK2',
+      'BARD1',
+      'RAD51D',
+      'RAD51C',
+      'BRIP1',
+      'ER:PR:HER2:CK14:CK56'
+    ])
+  ]);
+  var familyID = 'OPENPED';
+
+  const generateLine = (individual) => {
+    return generateDataLine([
+      individual.familyId ?? 'OPENPED',
+      (individual.name && individual.name.length > 0)
+        ? individual.name
+        : 'Unknown',
+      individual.target ?? 0,
+      individual.id,
+      individual.father ?? 0,
+      individual.mother ?? 0,
+      individual.sex ?? 'F',
+      individual.mzTwin ?? 0,
+      individual.dead ?? 0,
+      individual.age ?? 0,
+      individual.yob ?? 0,
+      individual.bc1 ?? 0,
+      individual.bc2 ?? 0,
+      individual.oc ?? 0,
+      individual.pro ?? 0,
+      individual.pan ?? 0,
+      individual.ashkn ?? 0,
+      individual.brca1 ?? '0:0',
+      individual.brca2 ?? '0:0',
+      individual.palb2 ?? '0:0',
+      individual.atm ?? '0:0',
+      individual.chek2 ?? '0:0',
+      individual.bard1 ?? '0:0',
+      individual.rad51d ?? '0:0',
+      individual.rad51c ?? '0:0',
+      individual.brip1 ?? '0:0',
+      individual.estrogen ?? '0:0:0:0:0'
+    ]) + '\n';
+  }
+  var output = HEADER;
+
+  var idToPedId = PedigreeExport.createNewIDs(pedigree, idGenerationPreference);
+
+  for (var i = 0; i <= pedigree.GG.getMaxRealVertexId(); i++) {
+    if (!pedigree.GG.isPerson(i)) {
+      continue;
+    }
+
+    let individual = {
+      id: idToPedId[i],
+      familyId: familyID,
+      name: [
+        pedigree.GG.properties[i]['fName'],
+        pedigree.GG.properties[i]['lName'],
+      ].filter(val => !!val)
+        .join('')
+        .substring(0, 7),
+    };
+
+    // mother & father
+    var parents = pedigree.GG.getParents(i);
+    if (parents.length > 0) {
+      const fatherIsLeftParent = pedigree.GG.properties[parents[0]]['gender'] == 'M'
+        || pedigree.GG.properties[parents[1]]['gender'] == 'F';
+
+      const father = fatherIsLeftParent ? parents[0] : parents[1];
+      const mother = fatherIsLeftParent ? parents[1] : parents[0];
+
+      individual.father = idToPedId[father];
+      individual.mother = idToPedId[mother];
+    }
+
+    if (pedigree.GG.properties[i]['gender'] == 'M') {
+      individual.sex = 'M';
+    } else if (pedigree.GG.properties[i]['gender'] == 'F') {
+      individual.sex = 'F';
+    }
+
+    if (pedigree.GG.properties[i]['monozygotic'] === true) {
+      individual.mzTwin = getTwinGroup(pedigree.GG.properties[i]);
+    }
+
+    const dob = pedigree.GG.properties[i]['dob'];
+    const dobData = new Date(pedigree.GG.properties[i]['dob']);
+    individual.dead = pedigree.GG.properties[i]['lifeStatus'] == 'deceased' ? 1 : 0;
+    individual.age = dob
+      ? getAge(dobData)
+      : 0;
+    individual.yob = dob
+      ? dobData.getFullYear()
+      : 0;
+    
+    output += generateLine(individual);
+  }
+
+  return output;
+};
+
 /* ===============================================================================================
  *
  * Creates and returns a JSON in the "GA4GH FHIR JSON" format
@@ -382,6 +563,7 @@ PedigreeExport.createNewIDs = function(pedigree, idGenerationPreference, maxLeng
     if (!pedigree.GG.isPerson(i)) {
       continue;
     }
+    console.log(pedigree.GG.properties[i]);
 
     var id = nextUnusedID++;
     if (idGenerationPreference == 'external' && pedigree.GG.properties[i].hasOwnProperty('externalID')) {
