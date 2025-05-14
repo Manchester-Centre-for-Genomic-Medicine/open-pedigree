2a00c5ab81c7ad014b97ffe4259677bdeff1f305
Algy Taylor <algy.taylor@mft.nhs.uk>
2025-05-14 11:23:52 +0100

    gh-1020: add canrisk as export option
    Update the export selector component to add `CanRisk 3.0` as an export
    option

--- src/script/view/exportSelector.js
+++ src/script/view/exportSelector.js
@@ -26,6 +26,7 @@ var ExportSelector = Class.create( {
     };
     var typeListElement = new Element('table');
     typeListElement.insert(_addTypeOption(true,  'PED', 'ped'));
+    typeListElement.insert(_addTypeOption(false,  'CanRisk 3.0', 'canrisk'));
     typeListElement.insert(_addTypeOption(false,  'GA4GH FHIR', 'GA4GH'));
     typeListElement.insert(_addTypeOption(false,  'SVG', 'svg'));
     typeListElement.insert(_addTypeOption(false,  'PDF', 'pdf'));
@@ -135,6 +136,9 @@ var ExportSelector = Class.create( {
     if (exportType == 'ped') {
       pedOptionsTable.show();
       privacyOptionsTable.hide();
+    } else if (exportType == 'canrisk') {
+      pedOptionsTable.hide();
+      privacyOptionsTable.hide();
     } else {
       pedOptionsTable.hide();
       privacyOptionsTable.show();
@@ -168,7 +172,15 @@ var ExportSelector = Class.create( {
       saveTextAs(exportString, fileName);
     } else {
       var privacySetting = $$('input:checked[type=radio][name="privacy-options"]')[0].value;
-      if (exportType == 'GA4GH') {
+      if (exportType='canrisk') {
+        var idGenerationSetting = $$('input:checked[type=radio][name="ped-options"]')[0].value;
+        var exportString = PedigreeExport.exportAsCanrisk(editor.getGraph().DG, 'newid');
+        var fileName = 'canrisk.tsv';
+        var mimeType = 'text/plain';
+        // Uses FileSaver global
+        /* eslint-disable no-undef */
+        saveTextAs(exportString, fileName);
+      } else if (exportType == 'GA4GH') {
         var exportString = PedigreeExport.exportAsGA4GH(editor.getGraph().DG, privacySetting);
         var fileName = 'open-pedigree-GA4GH-fhir.json';
         var mimeType = 'application/fhir+json';
